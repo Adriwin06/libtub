Bo98/libbndl

Collapse libbndl::Formats internal namespace

src/formats/base.hpp
@@ -15,121 +15,118 @@
 #	define __has_builtin(x) 0
 #endif
 
-namespace libbndl
+namespace libbndl::Formats
 {
-	namespace Formats
+	inline unsigned long BitScanReverse(unsigned long input)
 	{
-		inline unsigned long BitScanReverse(unsigned long input)
-		{
-			unsigned long result;
+		unsigned long result;
 
 #if defined(_MSC_VER)
-			_BitScanReverse(&result, input);
+		_BitScanReverse(&result, input);
 #elif __has_builtin(__builtin_clzl) || defined(__GNUC__)
-			result = static_cast<unsigned long>(std::numeric_limits<unsigned long>::digits - 1 - __builtin_clzl(input));
+		result = static_cast<unsigned long>(std::numeric_limits<unsigned long>::digits - 1 - __builtin_clzl(input));
 #else
-			result = std::bit_width(input | 1U) - 1;
+		result = std::bit_width(input | 1U) - 1;
 #endif
 
-			return result;
-		}
-
-		using ResourceKey = std::pair<ResourceID, uint8_t>;
-
-		struct ResourceData
-		{
-			uint32_t uncompressedSize;
-			uint32_t uncompressedAlignment; // default depending on file type
-			uint32_t onDiskSize;
-			uint32_t onDiskAlignment;
-			std::unique_ptr<uint8_t[]> data;
-		};
-
-		struct ResourceEntry
-		{
-			std::array<ResourceData, 4> descriptors;
-
-			uint64_t importHash;
-
-			uint32_t importOffset;
-			uint32_t resourceType;
-			uint16_t importCount;
-		};
-
-		struct ResourceDebugDataEntry
-		{
-			std::string name;
-			std::string typeName;
-		};
-
-		struct ImportEntry
-		{
-			ResourceID resourceID;
-			uint32_t offset;
-		};
-
-		class Base
-		{
-		public:
-			Base() = default;
-			Base(uint16_t version, Platform platform, Flags flags);
-			Base(const Base &) = delete;
-			Base &operator=(const Base &) = delete;
-			Base(Base &&) = delete;
-			Base &operator=(Base &&) = delete;
-			virtual ~Base() = default;
-
-			virtual bool Load(binaryio::BinaryReader &reader) = 0;
-			virtual bool Save(binaryio::BinaryWriter &reader) = 0;
-
-			[[nodiscard]] virtual constexpr Magic GetMagic() const = 0;
-			[[nodiscard]] constexpr uint16_t GetVersion() const { return m_version; }
-			[[nodiscard]] constexpr Platform GetPlatform() const { return m_platform; }
-			[[nodiscard]] constexpr Flags GetFlags() const { return m_flags; }
-
-			[[nodiscard]] std::optional<ResourceDebugDataEntry> GetResourceDebugData(ResourceKey resourceKey) const;
-			[[nodiscard]] std::optional<uint32_t> GetResourceType(ResourceKey resourceKey) const;
-			[[nodiscard]] virtual std::optional<Resource> GetResource(ResourceKey resourceKey) const = 0;
-			[[nodiscard]] Buffer GetBinary(ResourceKey resourceKey, MemoryType memoryType) const;
-
-			bool AddResource(ResourceKey resourceKey, const Resource &data);
-			bool AddResourceDebugData(ResourceKey resourceID, std::string name, std::string typeName);
-
-			bool ReplaceResource(ResourceKey resourceKey, const Resource &data);
-
-			[[nodiscard]] uint32_t GetResourceCount() const { return static_cast<uint32_t>(m_entries.size()); }
-			[[nodiscard]] std::vector<ResourceID> GetResourceIDs() const;
-			[[nodiscard]] std::map<uint32_t, std::vector<ResourceID>> GetResourceIDsByType() const;
-			[[nodiscard]] std::vector<uint8_t> GetResourceStreamIndices(ResourceID resourceID) const;
-
-			[[nodiscard]] virtual ResourceID GetDefaultResourceID() const;
-			[[nodiscard]] virtual int32_t GetDefaultResourceStreamIndex() const;
-			[[nodiscard]] virtual std::string GetStreamName(uint8_t index) const;
-
-			[[nodiscard]] virtual std::vector<MemoryType> GetMemoryTypes() const;
-
-		protected:
-			static constexpr const uint8_t kStreamLimit = 4;
-
-			std::map<ResourceKey, ResourceEntry> m_entries;
-			std::map<ResourceKey, ResourceDebugDataEntry> m_debugDataEntries;
-
-			uint16_t m_version;
-			Platform m_platform;
-			Flags m_flags;
-
-			[[nodiscard]] virtual constexpr bool AppendsImportsToResource() const = 0;
-			[[nodiscard]] virtual bool IsValidPlatform() const;
-
-			[[nodiscard]] std::endian GetPlatformEndian() const;
-
-			void ParseDebugData(const std::string &rstXML);
-			[[nodiscard]] std::string GenerateDebugData() const;
-			[[nodiscard]] virtual std::vector<ResourceKey> SortedDebugDataKeys() const;
-			[[nodiscard]] virtual std::vector<std::pair<std::string, std::string>> GetDebugDataAttributes(const ResourceKey &resourceKey, const ResourceDebugDataEntry &debugData) const;
-
-			[[nodiscard]] static ImportEntry ReadImport(binaryio::BinaryReader &reader);
-			static void WriteImport(binaryio::BinaryWriter &writer, const Import &import);
-		};
+		return result;
 	}
+
+	using ResourceKey = std::pair<ResourceID, uint8_t>;
+
+	struct ResourceData
+	{
+		uint32_t uncompressedSize;
+		uint32_t uncompressedAlignment; // default depending on file type
+		uint32_t onDiskSize;
+		uint32_t onDiskAlignment;
+		std::unique_ptr<uint8_t[]> data;
+	};
+
+	struct ResourceEntry
+	{
+		std::array<ResourceData, 4> descriptors;
+
+		uint64_t importHash;
+
+		uint32_t importOffset;
+		uint32_t resourceType;
+		uint16_t importCount;
+	};
+
+	struct ResourceDebugDataEntry
+	{
+		std::string name;
+		std::string typeName;
+	};
+
+	struct ImportEntry
+	{
+		ResourceID resourceID;
+		uint32_t offset;
+	};
+
+	class Base
+	{
+	public:
+		Base() = default;
+		Base(uint16_t version, Platform platform, Flags flags);
+		Base(const Base &) = delete;
+		Base &operator=(const Base &) = delete;
+		Base(Base &&) = delete;
+		Base &operator=(Base &&) = delete;
+		virtual ~Base() = default;
+
+		virtual bool Load(binaryio::BinaryReader &reader) = 0;
+		virtual bool Save(binaryio::BinaryWriter &reader) = 0;
+
+		[[nodiscard]] virtual constexpr Magic GetMagic() const = 0;
+		[[nodiscard]] constexpr uint16_t GetVersion() const { return m_version; }
+		[[nodiscard]] constexpr Platform GetPlatform() const { return m_platform; }
+		[[nodiscard]] constexpr Flags GetFlags() const { return m_flags; }
+
+		[[nodiscard]] std::optional<ResourceDebugDataEntry> GetResourceDebugData(ResourceKey resourceKey) const;
+		[[nodiscard]] std::optional<uint32_t> GetResourceType(ResourceKey resourceKey) const;
+		[[nodiscard]] virtual std::optional<Resource> GetResource(ResourceKey resourceKey) const = 0;
+		[[nodiscard]] Buffer GetBinary(ResourceKey resourceKey, MemoryType memoryType) const;
+
+		bool AddResource(ResourceKey resourceKey, const Resource &data);
+		bool AddResourceDebugData(ResourceKey resourceID, std::string name, std::string typeName);
+
+		bool ReplaceResource(ResourceKey resourceKey, const Resource &data);
+
+		[[nodiscard]] uint32_t GetResourceCount() const { return static_cast<uint32_t>(m_entries.size()); }
+		[[nodiscard]] std::vector<ResourceID> GetResourceIDs() const;
+		[[nodiscard]] std::map<uint32_t, std::vector<ResourceID>> GetResourceIDsByType() const;
+		[[nodiscard]] std::vector<uint8_t> GetResourceStreamIndices(ResourceID resourceID) const;
+
+		[[nodiscard]] virtual ResourceID GetDefaultResourceID() const;
+		[[nodiscard]] virtual int32_t GetDefaultResourceStreamIndex() const;
+		[[nodiscard]] virtual std::string GetStreamName(uint8_t index) const;
+
+		[[nodiscard]] virtual std::vector<MemoryType> GetMemoryTypes() const;
+
+	protected:
+		static constexpr const uint8_t kStreamLimit = 4;
+
+		std::map<ResourceKey, ResourceEntry> m_entries;
+		std::map<ResourceKey, ResourceDebugDataEntry> m_debugDataEntries;
+
+		uint16_t m_version;
+		Platform m_platform;
+		Flags m_flags;
+
+		[[nodiscard]] virtual constexpr bool AppendsImportsToResource() const = 0;
+		[[nodiscard]] virtual bool IsValidPlatform() const;
+
+		[[nodiscard]] std::endian GetPlatformEndian() const;
+
+		void ParseDebugData(const std::string &rstXML);
+		[[nodiscard]] std::string GenerateDebugData() const;
+		[[nodiscard]] virtual std::vector<ResourceKey> SortedDebugDataKeys() const;
+		[[nodiscard]] virtual std::vector<std::pair<std::string, std::string>> GetDebugDataAttributes(const ResourceKey &resourceKey, const ResourceDebugDataEntry &debugData) const;
+
+		[[nodiscard]] static ImportEntry ReadImport(binaryio::BinaryReader &reader);
+		static void WriteImport(binaryio::BinaryWriter &writer, const Import &import);
+	};
 }

src/formats/bnd2.hpp
@@ -1,41 +1,38 @@
 #pragma once
 #include "base.hpp"
 
-namespace libbndl
+namespace libbndl::Formats
 {
-	namespace Formats
+	class Bnd2 : public Base
 	{
-		class Bnd2 : public Base
-		{
-		public:
-			using Base::Base;
+	public:
+		using Base::Base;
 
-			bool Load(binaryio::BinaryReader &reader) override;
-			bool Save(binaryio::BinaryWriter &reader) override;
+		bool Load(binaryio::BinaryReader &reader) override;
+		bool Save(binaryio::BinaryWriter &reader) override;
 
-			[[nodiscard]] constexpr Magic GetMagic() const override { return Magic::Bnd2; }
+		[[nodiscard]] constexpr Magic GetMagic() const override { return Magic::Bnd2; }
 
-			[[nodiscard]] std::optional<Resource> GetResource(ResourceKey resourceKey) const override;
+		[[nodiscard]] std::optional<Resource> GetResource(ResourceKey resourceKey) const override;
 
-			[[nodiscard]] ResourceID GetDefaultResourceID() const override;
-			[[nodiscard]] int32_t GetDefaultResourceStreamIndex() const override;
-			[[nodiscard]] std::string GetStreamName(uint8_t index) const override;
+		[[nodiscard]] ResourceID GetDefaultResourceID() const override;
+		[[nodiscard]] int32_t GetDefaultResourceStreamIndex() const override;
+		[[nodiscard]] std::string GetStreamName(uint8_t index) const override;
 
-			[[nodiscard]] std::vector<MemoryType> GetMemoryTypes() const override;
+		[[nodiscard]] std::vector<MemoryType> GetMemoryTypes() const override;
 
-		protected:
-			[[nodiscard]] constexpr bool AppendsImportsToResource() const override { return false; }
-			[[nodiscard]] bool IsValidPlatform() const override;
+	protected:
+		[[nodiscard]] constexpr bool AppendsImportsToResource() const override { return false; }
+		[[nodiscard]] bool IsValidPlatform() const override;
 
-			[[nodiscard]] std::vector<ResourceKey> SortedDebugDataKeys() const override;
-			[[nodiscard]] std::vector<std::pair<std::string, std::string>> GetDebugDataAttributes(const ResourceKey &resourceKey, const ResourceDebugDataEntry &debugData) const override;
+		[[nodiscard]] std::vector<ResourceKey> SortedDebugDataKeys() const override;
+		[[nodiscard]] std::vector<std::pair<std::string, std::string>> GetDebugDataAttributes(const ResourceKey &resourceKey, const ResourceDebugDataEntry &debugData) const override;
 
-		private:
-			ResourceID m_defaultResourceID;
-			int32_t m_defaultResourceStreamIndex;
-			std::array<std::string, kStreamLimit> m_streamNames;
+	private:
+		ResourceID m_defaultResourceID;
+		int32_t m_defaultResourceStreamIndex;
+		std::array<std::string, kStreamLimit> m_streamNames;
 
-			[[nodiscard]] std::optional<uint8_t> MapFileBlockToLibBlock(uint8_t block) const;
-		};
-	}
+		[[nodiscard]] std::optional<uint8_t> MapFileBlockToLibBlock(uint8_t block) const;
+	};
 }

src/formats/bndl.hpp
@@ -1,28 +1,25 @@
 #pragma once
 #include "base.hpp"
 
-namespace libbndl
+namespace libbndl::Formats
 {
-	namespace Formats
+	class Bndl : public Base
 	{
-		class Bndl : public Base
-		{
-		public:
-			using Base::Base;
+	public:
+		using Base::Base;
 
-			bool Load(binaryio::BinaryReader &reader) override;
-			bool Save(binaryio::BinaryWriter &reader) override;
+		bool Load(binaryio::BinaryReader &reader) override;
+		bool Save(binaryio::BinaryWriter &reader) override;
 
-			[[nodiscard]] constexpr Magic GetMagic() const override { return Magic::Bndl; }
+		[[nodiscard]] constexpr Magic GetMagic() const override { return Magic::Bndl; }
 
-			[[nodiscard]] std::optional<Resource> GetResource(ResourceKey resourceKey) const override;
+		[[nodiscard]] std::optional<Resource> GetResource(ResourceKey resourceKey) const override;
 
-		private:
-			std::map<ResourceID, std::vector<ImportEntry>> m_imports;
+	private:
+		std::map<ResourceID, std::vector<ImportEntry>> m_imports;
 
-			[[nodiscard]] constexpr bool AppendsImportsToResource() const override { return false; }
+		[[nodiscard]] constexpr bool AppendsImportsToResource() const override { return false; }
 
-			[[nodiscard]] std::optional<uint8_t> MapFileBlockToLibBlock(uint8_t block) const;
-		};
-	}
+		[[nodiscard]] std::optional<uint8_t> MapFileBlockToLibBlock(uint8_t block) const;
+	};
 }

Adriwin06/libtub

(cherry picked from commit 4bdb8b674b240ff79099fe37a935fab4974a3462)
Applied to libtub::Formats. Whitespace-only apart from the namespace
declarations.
Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>

src/formats/base.hpp
@@ -21,142 +21,139 @@
 #	define __has_builtin(x) 0
 #endif
 
-namespace libtub
+namespace libtub::Formats
 {
-	namespace Formats
+	inline unsigned long BitScanReverse(unsigned long input)
 	{
-		inline unsigned long BitScanReverse(unsigned long input)
-		{
-			if (input == 0)
-				return 0;
+		if (input == 0)
+			return 0;
 
-			unsigned long result;
+		unsigned long result;
 
 #if defined(_MSC_VER)
-			_BitScanReverse(&result, input);
+		_BitScanReverse(&result, input);
 #elif __has_builtin(__builtin_clzl) || defined(__GNUC__)
-			result = static_cast<unsigned long>(std::numeric_limits<unsigned long>::digits - 1 - __builtin_clzl(input));
+		result = static_cast<unsigned long>(std::numeric_limits<unsigned long>::digits - 1 - __builtin_clzl(input));
 #else
-			result = std::bit_width(input | 1U) - 1;
+		result = std::bit_width(input | 1U) - 1;
 #endif
 
-			return result;
-		}
-
-		using ResourceKey = std::pair<ResourceID, uint8_t>;
-
-		struct ResourceData
-		{
-			uint32_t uncompressedSize;
-			uint32_t uncompressedAlignment; // default depending on file type
-			uint32_t onDiskSize;
-			uint32_t onDiskAlignment;
-			std::unique_ptr<uint8_t[]> data;
-		};
-
-		struct ResourceEntry
-		{
-			std::array<ResourceData, 4> descriptors;
-
-			uint64_t importHash;
-
-			uint32_t importOffset;
-			uint32_t resourceType;
-			uint16_t importCount;
-		};
-
-		struct ResourceDebugDataEntry
-		{
-			std::string name;
-			std::string typeName;
-		};
-
-		struct ImportEntry
-		{
-			ResourceID resourceID;
-			uint32_t offset;
-			Import::ImportType type;
-		};
-
-		class Base
-		{
-		public:
-			Base() = default;
-			Base(uint16_t version, Platform platform, Flags flags);
-			Base(const Base &) = delete;
-			Base &operator=(const Base &) = delete;
-			Base(Base &&) = delete;
-			Base &operator=(Base &&) = delete;
-			virtual ~Base() = default;
-
-			virtual ErrorCode Load(binaryio::BinaryReader &reader) = 0;
-			virtual ErrorCode Save(binaryio::BinaryWriter &writer) = 0;
-
-			[[nodiscard]] virtual constexpr Magic GetMagic() const = 0;
-			[[nodiscard]] constexpr uint16_t GetVersion() const { return m_version; }
-			[[nodiscard]] constexpr Platform GetPlatform() const { return m_platform; }
-			[[nodiscard]] constexpr Flags GetFlags() const { return m_flags; }
-
-			[[nodiscard]] std::optional<ResourceDebugDataEntry> GetResourceDebugData(ResourceKey resourceKey) const;
-			[[nodiscard]] std::optional<uint32_t> GetResourceType(ResourceKey resourceKey) const;
-			// Returns nothing when the resource is missing or one of its populated blocks can't be decoded.
-			[[nodiscard]] virtual std::optional<Resource> GetResource(ResourceKey resourceKey) const = 0;
-			// Decodes a single block as GetResource would return it: an empty buffer when the block has no data,
-			// nothing when the resource is missing or the block can't be decoded.
-			[[nodiscard]] virtual std::optional<Buffer> GetResourceBinary(ResourceKey resourceKey, MemoryType memoryType) const;
-			[[nodiscard]] bool HasResource(ResourceKey resourceKey) const { return m_entries.contains(resourceKey); }
-
-			bool AddResource(ResourceKey resourceKey, const Resource &data);
-			bool AddResourceDebugData(ResourceKey resourceID, std::string name, std::string typeName);
-
-			bool ReplaceResource(ResourceKey resourceKey, const Resource &data);
-
-			[[nodiscard]] uint32_t GetResourceCount() const { return static_cast<uint32_t>(m_entries.size()); }
-			[[nodiscard]] std::vector<ResourceID> GetResourceIDs() const;
-			[[nodiscard]] std::vector<ResourceKey> GetResourceKeys() const;
-			[[nodiscard]] std::map<uint32_t, std::vector<ResourceID>> GetResourceIDsByType() const;
-			[[nodiscard]] std::vector<uint8_t> GetResourceStreamIndices(ResourceID resourceID) const;
-
-			[[nodiscard]] virtual ResourceID GetDefaultResourceID() const;
-			[[nodiscard]] virtual int32_t GetDefaultResourceStreamIndex() const;
-			[[nodiscard]] virtual std::string GetStreamName(uint8_t index) const;
-			virtual bool SetDefaultResource(ResourceKey resourceKey);
-			virtual bool SetStreamName(uint8_t index, const std::string &name);
-
-			[[nodiscard]] virtual std::vector<MemoryType> GetMemoryTypes() const;
-
-		protected:
-			static constexpr const uint8_t kStreamLimit = 4;
-			// A serialised import: 64-bit resource ID, 32-bit offset/kind, padded to 8 bytes.
-			static constexpr const size_t kImportEntrySize = 16;
-
-			std::map<ResourceKey, ResourceEntry> m_entries;
-			std::map<ResourceKey, ResourceDebugDataEntry> m_debugDataEntries;
-
-			uint16_t m_version;
-			Platform m_platform;
-			Flags m_flags;
-
-			[[nodiscard]] virtual constexpr bool AppendsImportsToResource() const = 0;
-			// Called by ReplaceResource for formats that keep imports outside the resource payload.
-			virtual void StoreSeparateImports(ResourceKey, const std::vector<Import> &) {}
-			[[nodiscard]] virtual bool IsValidPlatform() const;
-
-			[[nodiscard]] std::endian GetPlatformEndian() const;
-
-			// Decompresses (or copies) one stored block. Unlike an empty buffer, nothing means the block holds
-			// data that could not be decoded.
-			[[nodiscard]] std::optional<Buffer> DecodeBinary(ResourceKey resourceKey, MemoryType memoryType) const;
-
-			void ParseDebugData(const std::string &rstXML);
-			[[nodiscard]] std::string GenerateDebugData() const;
-			// Writes the NUL-terminated resource string table.
-			void WriteDebugData(binaryio::BinaryWriter &writer) const;
-			[[nodiscard]] virtual std::vector<ResourceKey> SortedDebugDataKeys() const;
-			[[nodiscard]] virtual std::vector<std::pair<std::string, std::string>> GetDebugDataAttributes(const ResourceKey &resourceKey, const ResourceDebugDataEntry &debugData) const;
-
-			[[nodiscard]] static ImportEntry ReadImport(binaryio::BinaryReader &reader);
-			static void WriteImport(binaryio::BinaryWriter &writer, const Import &import);
-		};
+		return result;
 	}
+
+	using ResourceKey = std::pair<ResourceID, uint8_t>;
+
+	struct ResourceData
+	{
+		uint32_t uncompressedSize;
+		uint32_t uncompressedAlignment; // default depending on file type
+		uint32_t onDiskSize;
+		uint32_t onDiskAlignment;
+		std::unique_ptr<uint8_t[]> data;
+	};
+
+	struct ResourceEntry
+	{
+		std::array<ResourceData, 4> descriptors;
+
+		uint64_t importHash;
+
+		uint32_t importOffset;
+		uint32_t resourceType;
+		uint16_t importCount;
+	};
+
+	struct ResourceDebugDataEntry
+	{
+		std::string name;
+		std::string typeName;
+	};
+
+	struct ImportEntry
+	{
+		ResourceID resourceID;
+		uint32_t offset;
+		Import::ImportType type;
+	};
+
+	class Base
+	{
+	public:
+		Base() = default;
+		Base(uint16_t version, Platform platform, Flags flags);
+		Base(const Base &) = delete;
+		Base &operator=(const Base &) = delete;
+		Base(Base &&) = delete;
+		Base &operator=(Base &&) = delete;
+		virtual ~Base() = default;
+
+		virtual ErrorCode Load(binaryio::BinaryReader &reader) = 0;
+		virtual ErrorCode Save(binaryio::BinaryWriter &writer) = 0;
+
+		[[nodiscard]] virtual constexpr Magic GetMagic() const = 0;
+		[[nodiscard]] constexpr uint16_t GetVersion() const { return m_version; }
+		[[nodiscard]] constexpr Platform GetPlatform() const { return m_platform; }
+		[[nodiscard]] constexpr Flags GetFlags() const { return m_flags; }
+
+		[[nodiscard]] std::optional<ResourceDebugDataEntry> GetResourceDebugData(ResourceKey resourceKey) const;
+		[[nodiscard]] std::optional<uint32_t> GetResourceType(ResourceKey resourceKey) const;
+		// Returns nothing when the resource is missing or one of its populated blocks can't be decoded.
+		[[nodiscard]] virtual std::optional<Resource> GetResource(ResourceKey resourceKey) const = 0;
+		// Decodes a single block as GetResource would return it: an empty buffer when the block has no data,
+		// nothing when the resource is missing or the block can't be decoded.
+		[[nodiscard]] virtual std::optional<Buffer> GetResourceBinary(ResourceKey resourceKey, MemoryType memoryType) const;
+		[[nodiscard]] bool HasResource(ResourceKey resourceKey) const { return m_entries.contains(resourceKey); }
+
+		bool AddResource(ResourceKey resourceKey, const Resource &data);
+		bool AddResourceDebugData(ResourceKey resourceID, std::string name, std::string typeName);
+
+		bool ReplaceResource(ResourceKey resourceKey, const Resource &data);
+
+		[[nodiscard]] uint32_t GetResourceCount() const { return static_cast<uint32_t>(m_entries.size()); }
+		[[nodiscard]] std::vector<ResourceID> GetResourceIDs() const;
+		[[nodiscard]] std::vector<ResourceKey> GetResourceKeys() const;
+		[[nodiscard]] std::map<uint32_t, std::vector<ResourceID>> GetResourceIDsByType() const;
+		[[nodiscard]] std::vector<uint8_t> GetResourceStreamIndices(ResourceID resourceID) const;
+
+		[[nodiscard]] virtual ResourceID GetDefaultResourceID() const;
+		[[nodiscard]] virtual int32_t GetDefaultResourceStreamIndex() const;
+		[[nodiscard]] virtual std::string GetStreamName(uint8_t index) const;
+		virtual bool SetDefaultResource(ResourceKey resourceKey);
+		virtual bool SetStreamName(uint8_t index, const std::string &name);
+
+		[[nodiscard]] virtual std::vector<MemoryType> GetMemoryTypes() const;
+
+	protected:
+		static constexpr const uint8_t kStreamLimit = 4;
+		// A serialised import: 64-bit resource ID, 32-bit offset/kind, padded to 8 bytes.
+		static constexpr const size_t kImportEntrySize = 16;
+
+		std::map<ResourceKey, ResourceEntry> m_entries;
+		std::map<ResourceKey, ResourceDebugDataEntry> m_debugDataEntries;
+
+		uint16_t m_version;
+		Platform m_platform;
+		Flags m_flags;
+
+		[[nodiscard]] virtual constexpr bool AppendsImportsToResource() const = 0;
+		// Called by ReplaceResource for formats that keep imports outside the resource payload.
+		virtual void StoreSeparateImports(ResourceKey, const std::vector<Import> &) {}
+		[[nodiscard]] virtual bool IsValidPlatform() const;
+
+		[[nodiscard]] std::endian GetPlatformEndian() const;
+
+		// Decompresses (or copies) one stored block. Unlike an empty buffer, nothing means the block holds
+		// data that could not be decoded.
+		[[nodiscard]] std::optional<Buffer> DecodeBinary(ResourceKey resourceKey, MemoryType memoryType) const;
+
+		void ParseDebugData(const std::string &rstXML);
+		[[nodiscard]] std::string GenerateDebugData() const;
+		// Writes the NUL-terminated resource string table.
+		void WriteDebugData(binaryio::BinaryWriter &writer) const;
+		[[nodiscard]] virtual std::vector<ResourceKey> SortedDebugDataKeys() const;
+		[[nodiscard]] virtual std::vector<std::pair<std::string, std::string>> GetDebugDataAttributes(const ResourceKey &resourceKey, const ResourceDebugDataEntry &debugData) const;
+
+		[[nodiscard]] static ImportEntry ReadImport(binaryio::BinaryReader &reader);
+		static void WriteImport(binaryio::BinaryWriter &writer, const Import &import);
+	};
 }

src/formats/bnd2.hpp
@@ -1,44 +1,41 @@
 #pragma once
 #include "base.hpp"
 
-namespace libtub
+namespace libtub::Formats
 {
-	namespace Formats
+	class Bnd2 : public Base
 	{
-		class Bnd2 : public Base
-		{
-		public:
-			using Base::Base;
+	public:
+		using Base::Base;
 
-			ErrorCode Load(binaryio::BinaryReader &reader) override;
-			ErrorCode Save(binaryio::BinaryWriter &writer) override;
+		ErrorCode Load(binaryio::BinaryReader &reader) override;
+		ErrorCode Save(binaryio::BinaryWriter &writer) override;
 
-			[[nodiscard]] constexpr Magic GetMagic() const override { return Magic::Bnd2; }
+		[[nodiscard]] constexpr Magic GetMagic() const override { return Magic::Bnd2; }
 
-			[[nodiscard]] std::optional<Resource> GetResource(ResourceKey resourceKey) const override;
-			[[nodiscard]] std::optional<Buffer> GetResourceBinary(ResourceKey resourceKey, MemoryType memoryType) const override;
+		[[nodiscard]] std::optional<Resource> GetResource(ResourceKey resourceKey) const override;
+		[[nodiscard]] std::optional<Buffer> GetResourceBinary(ResourceKey resourceKey, MemoryType memoryType) const override;
 
-			[[nodiscard]] ResourceID GetDefaultResourceID() const override;
-			[[nodiscard]] int32_t GetDefaultResourceStreamIndex() const override;
-			[[nodiscard]] std::string GetStreamName(uint8_t index) const override;
-			bool SetDefaultResource(ResourceKey resourceKey) override;
-			bool SetStreamName(uint8_t index, const std::string &name) override;
+		[[nodiscard]] ResourceID GetDefaultResourceID() const override;
+		[[nodiscard]] int32_t GetDefaultResourceStreamIndex() const override;
+		[[nodiscard]] std::string GetStreamName(uint8_t index) const override;
+		bool SetDefaultResource(ResourceKey resourceKey) override;
+		bool SetStreamName(uint8_t index, const std::string &name) override;
 
-			[[nodiscard]] std::vector<MemoryType> GetMemoryTypes() const override;
+		[[nodiscard]] std::vector<MemoryType> GetMemoryTypes() const override;
 
-		protected:
-			[[nodiscard]] constexpr bool AppendsImportsToResource() const override { return true; }
-			[[nodiscard]] bool IsValidPlatform() const override;
+	protected:
+		[[nodiscard]] constexpr bool AppendsImportsToResource() const override { return true; }
+		[[nodiscard]] bool IsValidPlatform() const override;
 
-			[[nodiscard]] std::vector<ResourceKey> SortedDebugDataKeys() const override;
-			[[nodiscard]] std::vector<std::pair<std::string, std::string>> GetDebugDataAttributes(const ResourceKey &resourceKey, const ResourceDebugDataEntry &debugData) const override;
+		[[nodiscard]] std::vector<ResourceKey> SortedDebugDataKeys() const override;
+		[[nodiscard]] std::vector<std::pair<std::string, std::string>> GetDebugDataAttributes(const ResourceKey &resourceKey, const ResourceDebugDataEntry &debugData) const override;
 
-		private:
-			ResourceID m_defaultResourceID{};
-			int32_t m_defaultResourceStreamIndex = -1;
-			std::array<std::string, kStreamLimit> m_streamNames{};
+	private:
+		ResourceID m_defaultResourceID{};
+		int32_t m_defaultResourceStreamIndex = -1;
+		std::array<std::string, kStreamLimit> m_streamNames{};
 
-			[[nodiscard]] std::optional<uint8_t> MapFileBlockToLibBlock(uint8_t block) const;
-		};
-	}
+		[[nodiscard]] std::optional<uint8_t> MapFileBlockToLibBlock(uint8_t block) const;
+	};
 }

src/formats/bndl.hpp
@@ -1,29 +1,26 @@
 #pragma once
 #include "base.hpp"
 
-namespace libtub
+namespace libtub::Formats
 {
-	namespace Formats
+	class Bndl : public Base
 	{
-		class Bndl : public Base
-		{
-		public:
-			using Base::Base;
+	public:
+		using Base::Base;
 
-			ErrorCode Load(binaryio::BinaryReader &reader) override;
-			ErrorCode Save(binaryio::BinaryWriter &writer) override;
+		ErrorCode Load(binaryio::BinaryReader &reader) override;
+		ErrorCode Save(binaryio::BinaryWriter &writer) override;
 
-			[[nodiscard]] constexpr Magic GetMagic() const override { return Magic::Bndl; }
+		[[nodiscard]] constexpr Magic GetMagic() const override { return Magic::Bndl; }
 
-			[[nodiscard]] std::optional<Resource> GetResource(ResourceKey resourceKey) const override;
+		[[nodiscard]] std::optional<Resource> GetResource(ResourceKey resourceKey) const override;
 
-		private:
-			std::map<ResourceID, std::vector<ImportEntry>> m_imports;
+	private:
+		std::map<ResourceID, std::vector<ImportEntry>> m_imports;
 
-			[[nodiscard]] constexpr bool AppendsImportsToResource() const override { return false; }
-			void StoreSeparateImports(ResourceKey resourceKey, const std::vector<Import> &imports) override;
+		[[nodiscard]] constexpr bool AppendsImportsToResource() const override { return false; }
+		void StoreSeparateImports(ResourceKey resourceKey, const std::vector<Import> &imports) override;
 
-			[[nodiscard]] std::optional<uint8_t> MapFileBlockToLibBlock(uint8_t block) const;
-		};
-	}
+		[[nodiscard]] std::optional<uint8_t> MapFileBlockToLibBlock(uint8_t block) const;
+	};
 }