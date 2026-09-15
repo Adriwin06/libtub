Bo98/libbndl

Update some std::string parameter usage

include/libbndl/bundle.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <libbndl/internal/export.h>
 #include <array>
+#include <filesystem>
 #include <map>
 #include <memory>
 #include <optional>
@@ -150,7 +151,7 @@ namespace libbndl
 		constexpr ResourceID() noexcept : m_id(0) {}
 		constexpr ResourceID(uint32_t id, uint16_t type, uint8_t index, IDType idType) noexcept
 			: m_id(id | (static_cast<uint64_t>(type) << 32) | (static_cast<uint64_t>(index) << 48) || (static_cast<uint64_t>(idType) << 56)) {}
-		LIBBNDL_EXPORT explicit ResourceID(const std::string &name) noexcept;
+		LIBBNDL_EXPORT explicit ResourceID(std::string name) noexcept;
 		constexpr explicit ResourceID(UnderlyingType id) noexcept : m_id(id) {}
 
 		[[nodiscard]] constexpr bool operator==(const ResourceID &id) const noexcept = default;
@@ -195,8 +196,8 @@ namespace libbndl
 
 		constexpr Import(ResourceID resourceID, uint32_t offset, ImportType type = ImportType::Pointer) noexcept
 			: m_resourceID(resourceID), m_offset(offset | (static_cast<uint32_t>(type) << 31)) {}
-		Import(const std::string &resourceName, uint32_t offset, ImportType type = ImportType::Pointer) noexcept
-			: Import(ResourceID(resourceName), offset, type) {}
+		Import(std::string resourceName, uint32_t offset, ImportType type = ImportType::Pointer) noexcept
+			: Import(ResourceID(std::move(resourceName)), offset, type) {}
 
 		[[nodiscard]] constexpr ResourceID GetResourceID() const noexcept { return m_resourceID; }
 		[[nodiscard]] constexpr uint32_t GetOffset() const noexcept { return m_offset & 0x7FFFFFFF; }
@@ -273,8 +274,8 @@ namespace libbndl
 		LIBBNDL_EXPORT Bundle(Magic magic, uint16_t version, Platform platform, Flags flags);
 		LIBBNDL_EXPORT ~Bundle();
 
-		LIBBNDL_EXPORT bool Load(const std::string &name);
-		LIBBNDL_EXPORT bool Save(const std::string &name);
+		LIBBNDL_EXPORT bool Load(const std::filesystem::path &name);
+		LIBBNDL_EXPORT bool Save(const std::filesystem::path &name);
 
 		[[nodiscard]] LIBBNDL_EXPORT Magic GetMagic() const;
 		[[nodiscard]] LIBBNDL_EXPORT uint16_t GetVersion() const;

Adriwin06/libtub

(cherry picked from commit 21c6fffb69371ba79b89987d8f9ddb9d69e2c880)
Ported to libtub:
- Bundle::Load/Save already had std::filesystem::path overloads, so the
  std::string overloads are removed. Strings still convert implicitly.
- ResourceID now lower-cases its by-value argument in place, replacing
  libtub's chunked hashing, which only existed to avoid a copy.
- Upstream's std::move on the const debug data getters is left out; the
  next upstream commit reverts it.
- README lists the path-based Load/Save signatures.
Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>

include/libtub/bundle.hpp
@@ -177,7 +177,7 @@ namespace libtub
 		constexpr ResourceID() noexcept : m_id(0) {}
 		constexpr ResourceID(uint32_t id, uint16_t type, uint8_t index, IDType idType) noexcept
 			: m_id(id | (static_cast<uint64_t>(type) << 32) | (static_cast<uint64_t>(index) << 48) | (static_cast<uint64_t>(idType) << 56)) {}
-		LIBTUB_EXPORT explicit ResourceID(const std::string &name) noexcept;
+		LIBTUB_EXPORT explicit ResourceID(std::string name) noexcept;
 		constexpr explicit ResourceID(UnderlyingType id) noexcept : m_id(id) {}
 
 		[[nodiscard]] constexpr bool operator==(const ResourceID &id) const noexcept = default;
@@ -222,8 +222,8 @@ namespace libtub
 
 		constexpr Import(ResourceID resourceID, uint32_t offset, ImportType type = ImportType::Pointer) noexcept
 			: m_resourceID(resourceID), m_offset(offset | (static_cast<uint32_t>(type) << 31)) {}
-		Import(const std::string &resourceName, uint32_t offset, ImportType type = ImportType::Pointer) noexcept
-			: Import(ResourceID(resourceName), offset, type) {}
+		Import(std::string resourceName, uint32_t offset, ImportType type = ImportType::Pointer) noexcept
+			: Import(ResourceID(std::move(resourceName)), offset, type) {}
 
 		[[nodiscard]] constexpr ResourceID GetResourceID() const noexcept { return m_resourceID; }
 		[[nodiscard]] constexpr uint32_t GetOffset() const noexcept { return m_offset & 0x7FFFFFFF; }
@@ -333,10 +333,8 @@ namespace libtub
 		[[nodiscard]] LIBTUB_EXPORT const std::string &GetLastErrorMessage() const noexcept;
 		LIBTUB_EXPORT void ClearLastError() const;
 
-		LIBTUB_EXPORT bool Load(const std::string &name);
 		LIBTUB_EXPORT bool Load(const std::filesystem::path &path);
 		LIBTUB_EXPORT bool Load(std::span<const uint8_t> data);
-		LIBTUB_EXPORT bool Save(const std::string &name);
 		LIBTUB_EXPORT bool Save(const std::filesystem::path &path);
 		[[nodiscard]] LIBTUB_EXPORT std::vector<uint8_t> SaveToMemory();
 