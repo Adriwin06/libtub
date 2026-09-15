Bo98/libbndl

Set up clang-tidy and MS Code Analysis

src/bundle_c.cpp
@@ -211,7 +211,7 @@ libbndl_error libbndl_get_resource_type(const libbndl_bundle *LIBBNDL_NONNULL bu
 struct libbndl_buffer : public Buffer
 {
 	using Buffer::Buffer;
-	LIBBNDL_DEFAULT_MOVE_CONSTEXPR libbndl_buffer(Buffer &&buffer) : Buffer(std::move(buffer)) {}
+	LIBBNDL_BUFFER_CONSTEXPR libbndl_buffer(Buffer &&buffer) : Buffer(std::move(buffer)) {}
 };
 
 libbndl_error libbndl_buffer_create(libbndl_buffer *LIBBNDL_NULLABLE *LIBBNDL_NONNULL buffer, const void *LIBBNDL_NONNULL data, size_t size, uint32_t alignment)
@@ -270,7 +270,7 @@ uint32_t libbndl_buffer_get_alignment(const libbndl_buffer *LIBBNDL_NONNULL buff
 struct libbndl_import : public Import
 {
 	using Import::Import;
-	constexpr libbndl_import(Import &&import) : Import(std::move(import)) {}
+	constexpr libbndl_import(const Import &import) : Import(import) {}
 };
 
 libbndl_error libbndl_import_create(libbndl_import *LIBBNDL_NULLABLE *LIBBNDL_NONNULL import, libbndl_resource_id resourceID, uint32_t offset, libbndl_import_type importType)
@@ -307,7 +307,7 @@ libbndl_import_type libbndl_import_get_import_type(const libbndl_import *LIBBNDL
 struct libbndl_resource : public Resource
 {
 	using Resource::Resource;
-	LIBBNDL_DEFAULT_MOVE_CONSTEXPR libbndl_resource(Resource &&resource) : Resource(std::move(resource)) {}
+	libbndl_resource(Resource &&resource) : Resource(std::move(resource)) {}
 };
 
 libbndl_error libbndl_resource_create(libbndl_resource *LIBBNDL_NULLABLE *LIBBNDL_NONNULL resource, libbndl_resource_type resourceType)
@@ -374,7 +374,7 @@ libbndl_error libbndl_resource_copy_import(const libbndl_resource *LIBBNDL_NONNU
 	if (index >= imports.size())
 		return LIBBNDL_ERROR_OUT_OF_RANGE;
 
-	*import = new libbndl_import(std::move(imports[index]));
+	*import = new libbndl_import(imports[index]);
 	if (*import == nullptr)
 		return LIBBNDL_ERROR_MEMORY_ALLOCATION;
 
@@ -446,7 +446,7 @@ libbndl_error libbndl_get_resource_id_at_index(const libbndl_bundle *LIBBNDL_NON
 bool libbndl_is_populated_resource_stream_index(const libbndl_bundle *LIBBNDL_NONNULL bundle, libbndl_resource_id resourceID, uint8_t streamIndex)
 {
 	const auto indices = bundle->GetResourceStreamIndices(ResourceID(resourceID));
-	return std::find(indices.begin(), indices.end(), streamIndex) != indices.end();
+	return std::ranges::find(indices, streamIndex) != indices.end();
 }
 
 libbndl_resource_id libbndl_get_default_resource_id(const libbndl_bundle *LIBBNDL_NONNULL bundle)
@@ -470,5 +470,5 @@ libbndl_error libbndl_get_stream_name(const libbndl_bundle *LIBBNDL_NONNULL bund
 bool libbndl_is_valid_memory_type(const libbndl_bundle *LIBBNDL_NONNULL bundle, libbndl_memory_type memoryType)
 {
 	const auto memoryTypes = bundle->GetMemoryTypes();
-	return std::find(memoryTypes.begin(), memoryTypes.end(), static_cast<MemoryType>(memoryType)) != memoryTypes.end();
+	return std::ranges::find(memoryTypes, static_cast<MemoryType>(memoryType)) != memoryTypes.end();
 }

Adriwin06/libtub

(cherry picked from commit b28443beb4163913179a973bce479197ffbf332c)
Ported to libtub:
- libbndl.ruleset is named libtub.ruleset, and the Visual Studio code
  analysis settings live in libtub's base preset.
- The Qt presets checksum change is omitted; upstream removes those
  presets two commits later.
- libtub's ResourceDebugData and Resource getters already return const
  references, so upstream's by-value getter changes are skipped, as are
  the Bundle move members libtub already declares.
- The same override/nodiscard cleanup covers libtub-only overrides
  (GetResourceBinary, SetDefaultResource, SetStreamName,
  StoreSeparateImports), and std::endl is replaced in libtub-only
  bndl_util output as well.
- ReadImport keeps libtub's import kind field in its designated
  initializer.
Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>

src/bundle_c.cpp
@@ -355,7 +355,7 @@ libtub_error libtub_get_resource_type(const libtub_bundle *LIBTUB_NONNULL bundle
 struct libtub_buffer : public Buffer
 {
 	using Buffer::Buffer;
-	LIBTUB_DEFAULT_MOVE_CONSTEXPR libtub_buffer(Buffer &&buffer) : Buffer(std::move(buffer)) {}
+	LIBTUB_BUFFER_CONSTEXPR libtub_buffer(Buffer &&buffer) : Buffer(std::move(buffer)) {}
 };
 
 libtub_error libtub_buffer_create(libtub_buffer *LIBTUB_NULLABLE *LIBTUB_NONNULL buffer, const void *LIBTUB_NONNULL data, size_t size, uint32_t alignment)
@@ -436,7 +436,7 @@ uint32_t libtub_buffer_get_alignment(const libtub_buffer *LIBTUB_NONNULL buffer)
 struct libtub_import : public Import
 {
 	using Import::Import;
-	constexpr libtub_import(Import &&import) : Import(std::move(import)) {}
+	constexpr libtub_import(const Import &import) : Import(import) {}
 };
 
 libtub_error libtub_import_create(libtub_import *LIBTUB_NULLABLE *LIBTUB_NONNULL import, libtub_resource_id resourceID, uint32_t offset, libtub_import_type importType)
@@ -489,7 +489,7 @@ libtub_import_type libtub_import_get_import_type(const libtub_import *LIBTUB_NON
 struct libtub_resource : public Resource
 {
 	using Resource::Resource;
-	LIBTUB_DEFAULT_MOVE_CONSTEXPR libtub_resource(Resource &&resource) : Resource(std::move(resource)) {}
+	libtub_resource(Resource &&resource) : Resource(std::move(resource)) {}
 };
 
 libtub_error libtub_resource_create(libtub_resource *LIBTUB_NULLABLE *LIBTUB_NONNULL resource, libtub_resource_type resourceType)
@@ -570,7 +570,7 @@ libtub_error libtub_resource_copy_import(const libtub_resource *LIBTUB_NONNULL r
 	if (index >= imports.size())
 		return LIBTUB_ERROR_OUT_OF_RANGE;
 
-	*import = new (std::nothrow) libtub_import(Import(imports[index]));
+	*import = new (std::nothrow) libtub_import(imports[index]);
 	if (*import == nullptr)
 		return LIBTUB_ERROR_MEMORY_ALLOCATION;
 
@@ -669,7 +669,7 @@ bool libtub_is_populated_resource_stream_index(const libtub_bundle *LIBTUB_NONNU
 
 	return GuardValue(false, [&] {
 		const auto indices = bundle->GetResourceStreamIndices(ResourceID(resourceID));
-		return std::find(indices.begin(), indices.end(), streamIndex) != indices.end();
+		return std::ranges::find(indices, streamIndex) != indices.end();
 	});
 }
 
@@ -704,6 +704,6 @@ bool libtub_is_valid_memory_type(const libtub_bundle *LIBTUB_NONNULL bundle, lib
 
 	return GuardValue(false, [&] {
 		const auto memoryTypes = bundle->GetMemoryTypes();
-		return std::find(memoryTypes.begin(), memoryTypes.end(), static_cast<MemoryType>(memoryType)) != memoryTypes.end();
+		return std::ranges::find(memoryTypes, static_cast<MemoryType>(memoryType)) != memoryTypes.end();
 	});
 }